beb96907aa0d032b640b3c249d105462874dcb3e
jana-200 <kamalgana2004@gmail.com>
2024-11-12 13:58:53 +0100

    init 2.6b

--- exercises/2.4b/2.4b/src/components/App/index.tsx
+++ exercises/2.4b/2.4b/src/components/App/index.tsx
@@ -10,9 +10,9 @@ export const App=() =>{
   return (
     <>
       <Header logo="https://images.unsplash.com/photo-1729432536160-d4ba057b61d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D" children="Bienvenu !"></Header>
-      <User nom="Jana" age={20} enLigne= {false} ></User>
-      <User nom="Marwa" age={19} enLigne= {true} ></User>
-      <User nom="Assia" age={19} enLigne= {false} ></User>
+      <User nom="Jana" age={20} enLigne= {true} ></User>
+      <User nom="lili" age={20} enLigne= {false} ></User>
+      <User nom="sousou" age={19} enLigne= {true} ></User>
 
       <Footer children="bye bye"></Footer>
     </>
