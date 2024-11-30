7b3c24cfac9a8e167f5bca7cee7adfb081e6a758
jana-200 <kamalgana2004@gmail.com>
2024-11-30 21:06:23 +0100

    new:ex2.14

--- exercises/2.13-14/2.13-14/src/App.tsx
+++ exercises/2.13-14/2.13-14/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [joke, setJoke] = useState<Joke>();
+  const [, setCount] = useState(0);
 
   interface Joke{
     category:string;
@@ -10,31 +11,34 @@ function App() {
 
   }
 
-
   useEffect(()=>{
-    fetch("https://v2.jokeapi.dev/joke/Any")
-    .then((response)=>{
-      if(!response.ok)
-        throw new Error(
-          `fetch error : ${response.status} : ${response.statusText}`);
+    const interval = setInterval(() => {
+      setCount(count => count + 1);
+      fetch("https://v2.jokeapi.dev/joke/Any")
+        .then((response) => {
+          if (!response.ok)
+            throw new Error(
+              `fetch error : ${response.status} : ${response.statusText}`
+            );
           return response.json();
         })
         .then((data) => {
           const joke = {
             category: data.category,
-            text: data.joke || `${data.setup} ... ${data.delivery}`
+            text: data.joke || `${data.setup} ... ${data.delivery}`,
           };
           setJoke(joke);
         })
-        .catch((err)=>{
+        .catch((err) => {
           console.error("HomePage::error: ", err);
         });
-
+    }, 10000);
+        return () => clearInterval(interval);
   }, []);
   return (
     
     <>
-      
+  
       {joke && (
         <div className="joke">
           <h2>Joke Category: {joke.category}</h2>
